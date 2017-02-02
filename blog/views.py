c378dc901c71dcfec0420fbd9581f6c65fe7916a
Hailo <jose.mallma@pucp.pe>
2017-02-02 11:24:37 -0500

    test

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.publish_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
