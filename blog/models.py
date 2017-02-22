38712a98ac171841dcc67576aaa156041d9f45aa
Hailo <jose.mallma@pucp.pe>
2017-02-22 17:31:27 -0500

    dgfsg

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 class Post(models.Model) :
 	author = models.ForeignKey('auth.User')
-	title = models.CharField(max_length = 200)
-	text = models.TextField()
+	title  = models.CharField(max_length = 200)
+	text   = models.TextField()
 	created_date = models.DateTimeField(
 		default = timezone.now)
 	publish_date = models.DateTimeField(
